05d8550cb4e185614d91224b507b8af333e549c0
Sergei Rashitov <sergei.rashitov@demlabs.net>
2020-03-19 05:50:56 -0400

    New package
    [+] Add initial structure of folders
    [+] Add libdap-nodejs package

---
+++ packages/libdap-nodejs/src/utils.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <node_api.h>
+
+// Check windows
+#if _WIN32 || _WIN64
+#if _WIN64
+#define ENV64
+#else
+#define ENV32
+#endif
+#endif
+
+// Check GCC
+#if __GNUC__
+#if __x86_64__ || __ppc64__
+#define ENV64
+#else
+#define ENV32
+#endif
+#endif
+
+
+#ifdef ENV64
+  #define napi_create_int napi_create_int64
+#else
+  #define napi_create_int napi_create_int32
+#endif
+
+
+
+#define DECLARE_NAPI_METHOD(name, func) \
+  { name, 0, func, 0, 0, 0, napi_default, 0 }
+
+
+/*!
+    Checks arguments count.
+
+    Expects declared variables: env, info
+    Declares variables: argc, args
+*/
+#define ARG_COUNT_CHECK_UNIQUE(__expected_count_const) \
+  size_t argc = (__expected_count_const); \
+  napi_value args[(__expected_count_const)]; \
+  status = napi_get_cb_info(env, info, &argc, args, nullptr, nullptr); \
+  assert(status == napi_ok); \
+  if (argc != (__expected_count_const)) { \
+    napi_throw_type_error(env, nullptr, "Wrong number of arguments"); \
+    return nullptr; \
+  }
+
+/*!
+    Checks argument type.
+
+    Expects declared variables: env, args, arg_type and status;
+
+    TODO: better error string based on argument's number and type;
+*/
+#define ARG_TYPE_CHECK(__arg_num, __arg_type) \
+  status = napi_typeof(env, args[(__arg_num)], &arg_type); \
+  assert(status == napi_ok); \
+  if (arg_type != (__arg_type)) { \
+    napi_throw_type_error(env, nullptr, "Wrong argument type"); \
+    return nullptr; \
+  }
+
+/*
+    WARNING: Type check is omitted!
+*/
+char* extract_str(napi_env env, napi_value js_value);
---
+++ packages/libdap-nodejs/test.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var dap = require("./index.js");
+
+var test_dir = './test.tmp/';
+var app_name = 'test_app';
+var log_file = test_dir + 'test.log';
+var config_file = test_dir + app_name + '.cfg';
+
+if (fs.existsSync(test_dir))
+{
+    fs.rmdirSync(test_dir, {recursive: true});
+}
+fs.mkdirSync(test_dir);
+fs.writeFileSync(config_file, "[server]\nlisten_address=0.0.0.0\n");
+
+
+dap.common.init(app_name, log_file);
+dap.config.init(test_dir);
+
+dap.logger.setLevel(0);
+dap.logger.setLevel("DeBUg");
+
+console.log('itoa(10) = ' + dap.itoa(10)); // '10'
+
+dap.logger.error("OOPS ERROR");
+dap.logger.critical("OOPS CRITICAL");
+dap.logger.logIt(3, "MSG KIND of a MESSAGE");
+
+var cfg = new dap.config.Config(app_name);
+var address = cfg.getString("server", "listen_address")
+console.log("address = " + address);
+var address_with_default = cfg.getString("server", "listen_address", "8.8.8.8")
+console.log("address_with_default = " + address_with_default);
+
+var missing_key = cfg.getString("unknown", "what?");
+var missing_key_with_default = cfg.getString("unknown", "what?", "default_value");
+console.log("missing_key = " + missing_key);
+console.log("missing_key_with_default = " + missing_key_with_default);
+
+dap.logger.debug("log", new String("message"), String("multiple"), 'arguments');
