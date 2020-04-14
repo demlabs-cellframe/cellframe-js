76b1b52658b35345b6d850656d4114f0c2eb7e18
Sergei Rashitov <sergei.rashitov@demlabs.net>
2020-04-14 10:54:30 +0000

    Wrapper for libdap-stream
    [+] New package: libdap-stream-nodejs

---
+++ packages/libdap-stream-nodejs/index.js
@@ -0,0 +1,25 @@
+var addon = require('bindings')('libdap-stream-nodejs');
+
+exports.stream = {
+    init: function stream_init(dumpPacketHeaders) {
+        return addon.dap_stream_init(dumpPacketHeaders);
+    },
+    deinit: function stream_deinit() {
+        return addon.dap_stream_deinit();
+    },
+    addProc: function stream_add_proc(server, urlComponent) {
+        return addon.dap_stream_add_proc_http(server, urlComponent);
+    },
+};
+
+exports.streamCtl = {
+    init: function stream_ctl_init(keyType, size) {
+        return addon.dap_stream_ctl_init(keyType, size);
+    },
+    deinit: function stream_ctl_deinit() {
+        return addon.dap_stream_ctl_deinit();
+    },
+    addProc: function stream_ctl_add_proc(server, urlComponent) {
+        return addon.dap_stream_ctl_add_proc(server, urlComponent);
+    },
+};
